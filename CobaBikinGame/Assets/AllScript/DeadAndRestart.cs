d9fae11a54f043254f88f1aec31f4b98611e27a0
PatrickJonathan11 <patrick.joenathan@gmail.com>
2016-11-08 14:04:34 +0700

    game fix

---
+++ CobaBikinGame/Assets/AllScript/DeadAndRestart.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadAndRestart : MonoBehaviour {
+	public Transform platformGenerator;
+	private Vector3 platformStartPoint;
+
+	public Movement thePlayer;
+	private Vector3 playerStartPoint;
+	// Use this for initialization
+	void Start () {
+		platformStartPoint = platformGenerator.position;
+		playerStartPoint = thePlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
